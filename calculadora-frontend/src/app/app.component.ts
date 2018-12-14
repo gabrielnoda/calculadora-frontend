8f6eb5ad07a49504e88c6767bc69e27f0aee0eb3
gabrielnoda <gabrielnoda@hotmail.com>
2018-12-14 16:26:34 -0200

    (DB1 Start) Calculadora Frontend

---
+++ calculadora-frontend/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  numeroUm: number = 2;
+  numeroDois: number = 10;
+  operacao: string = 'SOMA';
+
+  response: any = {};
+
+  constructor(private http: HttpClient){
+
+  }
+
+  onClickExercutarOperacao(){
+    const calculadora = {
+    operacao: this.operacao,
+    numeroUm: this.numeroUm,
+    numeroDois: this.numeroDois
+  };
+
+  this.http.post('https://calculadoragabrielnoda.herokuapp.com/api/calculadora',calculadora).subscribe(response =>this.response = response);
+  }
+}
